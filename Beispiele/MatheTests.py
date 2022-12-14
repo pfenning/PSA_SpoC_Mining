af4c64c985bd819e62df3c571c331f5031b73e03
Basti <basti.pilatus@pimail-home.de>
2022-12-14 19:13:53 +0100

    Fuzzy v2 Visualisieren und Analysieren

--- Beispiele/MatheTests.py
+++ Beispiele/MatheTests.py
@@ -38,53 +38,59 @@ import matplotlib.pyplot as plt
 #     rank_t_flug.append(sum(weights * sol))  # Bewertung aus gewichteter Summe
 # print(rank_t_flug)
 
-"""Überlegungen zur Auswahl von t_start aus t_start_var"""
-t_opt = 20
-# ToDo: Unterscheidung, ob gewartet, oder zu früh geflogen?? & t_var bei Wertepaaren noch definieren
-#       Ansätze: separate Betrachten, t_var < 0, noch mehr?
-#       Bisheriges Ergebnis: Bei negativen Werten immer zu gut bewertet
-# Beispielfälle:
-# 75% abgebaut, 90% abgebaut, optimal, +25 % Tage warten, +100 % Tage warten (-2 bis 2)
-# super Spritverbrauch, mäßiger Spritverbrauch, schlechter Spritverbrauch (0 bis 2)
-results_t_start = [[0, 800],    # 00:  0, 0
-                   [5, 800],    # 01:  1, 0
-                   [-2, 800],   # 02: -1, 0
-                   [0, 2000],   # 03:  0, 1
-                   [20, 800],   # 04:  2, 0
-                   [5, 2000],   # 05:  1, 1
-                   [-5, 800],   # 06: -2, 0
-                   [-2, 2000],  # 07: -1, 1
-                   [20, 2000],  # 08:  2, 1
-                   [0, 3500],   # 09:  0, 2
-                   [5, 3500],   # 10:  1, 2
-                   [-2, 3500],  # 11: -1, 2
-                   [-5, 2000],  # 12: -2, 1
-                   [20, 3500],  # 13:  2, 2
-                   [-5, 3500]]  # 14: -2, 2
-# Normierung der Werte
-# norm_t_start = t_opt
-norm_dv = 2000
-t_start_var = []
-for results in results_t_start:
-    t_start_var.append(results[0])
-    results[0] = abs(results[0]/(t_opt/2))
-    results[1] = results[1]/norm_dv
-print(results_t_start)
+# """Überlegungen zur Auswahl von t_start aus t_start_var"""
+# t_opt = 20
+# # ToDo: Unterscheidung, ob gewartet, oder zu früh geflogen?? & t_var bei Wertepaaren noch definieren
+# #       Ansätze: separate Betrachten, t_var < 0, noch mehr?
+# #       Bisheriges Ergebnis: Bei negativen Werten immer zu gut bewertet
+# # Beispielfälle:
+# # 75% abgebaut, 90% abgebaut, optimal, +25 % Tage warten, +100 % Tage warten (-2 bis 2)
+# # super Spritverbrauch, mäßiger Spritverbrauch, schlechter Spritverbrauch (0 bis 2)
+# results_t_start = [[0, 800],    # 00:  0, 0
+#                    [5, 800],    # 01:  1, 0
+#                    [-2, 800],   # 02: -1, 0
+#                    [0, 2000],   # 03:  0, 1
+#                    [20, 800],   # 04:  2, 0
+#                    [5, 2000],   # 05:  1, 1
+#                    [-5, 800],   # 06: -2, 0
+#                    [-2, 2000],  # 07: -1, 1
+#                    [20, 2000],  # 08:  2, 1
+#                    [0, 3500],   # 09:  0, 2
+#                    [5, 3500],   # 10:  1, 2
+#                    [-2, 3500],  # 11: -1, 2
+#                    [-5, 2000],  # 12: -2, 1
+#                    [20, 3500],  # 13:  2, 2
+#                    [-5, 3500]]  # 14: -2, 2
+# # Normierung der Werte
+# # norm_t_start = t_opt
+# norm_dv = 2000
+# t_start_var = []
+# for results in results_t_start:
+#     t_start_var.append(results[0])
+#     results[0] = abs(results[0]/(t_opt/2))
+#     results[1] = results[1]/norm_dv
+# print(results_t_start)
+#
+# # Gewichtete Summe
+# weights_neg_var = np.array([1.5, 0.7])
+# weights_pos_var = np.array([0.3, 0.7])
+# weights = weights_pos_var
+# rank_t_start = []
+# for i in range(0, len(results_t_start)):
+#     if t_start_var[i] < 0:
+#         weights = weights_neg_var
+#     else:
+#         weights = weights_pos_var
+#
+#     rank_t_start.append(sum(weights * results_t_start[i]))  # Bewertung aus gewichteter Summe
+# print(rank_t_start)
+#
+# plt.plot(rank_t_start)
+# plt.show()
 
-# Gewichtete Summe
-weights_neg_var = np.array([1.5, 0.7])
-weights_pos_var = np.array([0.3, 0.7])
-weights = weights_pos_var
-rank_t_start = []
-for i in range(0, len(results_t_start)):
-    if t_start_var[i] < 0:
-        weights = weights_neg_var
-    else:
-        weights = weights_pos_var
 
-    rank_t_start.append(sum(weights * results_t_start[i]))  # Bewertung aus gewichteter Summe
-print(rank_t_start)
-
-plt.plot(rank_t_start)
-plt.show()
-
+"""Minimum aus Matrix"""
+matrix = np.array([[3, 5, 1],
+          [4, 2, 5],
+          [9, 7, 3]])
+print(matrix.min())
--- Cluster_Verteilung.py
+++ Cluster_Verteilung.py
@@ -5,13 +5,11 @@ from pykep import epoch
 import matplotlib.pyplot as plt
 from scipy.stats import kde
 
-
-#Importieren von Mathekonstanten
+# Importieren von Mathekonstanten
 from math import pi
 
 # Konstanten und Database Laden
 from spoc_constants import data, asteroids
-
 
 ##########
 # Tests
@@ -20,7 +18,7 @@ from spoc_constants import data, asteroids
 # Analyse der Keplerparameter auf Cluster:
 # Creating Figure object
 fig = plt.figure()
-# Beschreibung der Elipse:
+# Beschreibung der Ellipse:
 semi_major = []
 eccentricity = []
 # Orientierung und Lage der Elipse in der Referenzebene
@@ -33,82 +31,77 @@ for line in data:
     # Beschreibung der Elipse:
     semi_major.append(line[1])
     eccentricity.append(line[2])
-    # Orientierung und Lage der Elipse in der Referenzebene
-    inclination.append(line[3]*pk.RAD2DEG)      # Umrechnung rad in Grad
-    ascending_node.append(line[4]*pk.RAD2DEG)
-    arg_of_perapsis.append(line[5]*pk.RAD2DEG)
+    # Orientierung und Lage der Ellipse in der Referenzebene
+    inclination.append(line[3] * pk.RAD2DEG)  # Umrechnung rad in Grad
+    ascending_node.append(line[4] * pk.RAD2DEG)
+    arg_of_perapsis.append(line[5] * pk.RAD2DEG)
 
 # Plotten der verschiedenen größen einzeln, zur Bestimmung der Dichte-Verteilungen
 
 # Semi-Major
 """ Summe von Perarpsis (kleinster Abstand der zwei Himmelskörper) und Apoapsis (größter Abstand) """
 prob_density = kde.gaussian_kde(semi_major)
-prob_density.covariance_factor = lambda : .15   # Glättung verringern (=kleinere Zahl)
+prob_density.covariance_factor = lambda: .15  # Glättung verringern (=kleinere Zahl)
 prob_density._compute_covariance()
 
-x = np.linspace(min(semi_major),max(semi_major),300)
+x = np.linspace(min(semi_major), max(semi_major), 300)
 y = prob_density(x)
-plt.subplot(2,3,1)
-plt.plot(x,y)
+plt.subplot(2, 3, 1)
+plt.plot(x, y)
 plt.title("Density of Semi-Major-Axis [m]")
-
 
 # Eccentricity
 """ Form der Elipse im Vergleich zum Keris """
 prob_density = kde.gaussian_kde(eccentricity)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-x = np.linspace(min(eccentricity),max(eccentricity),300)
+x = np.linspace(min(eccentricity), max(eccentricity), 300)
 y = prob_density(x)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-plt.subplot(2,3,2)
-plt.plot(x,y)
+plt.subplot(2, 3, 2)
+plt.plot(x, y)
 plt.title("Density of Eccentricity")
-
 
 # Inclination
 """ Neigung der Elipse an der Ascending Node (Schnittpunkt von Elipse und Referenzebene) """
 prob_density = kde.gaussian_kde(inclination)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-x = np.linspace(min(inclination),max(inclination),300)
+x = np.linspace(min(inclination), max(inclination), 300)
 y = prob_density(x)
-plt.subplot(2,3,3)
-plt.plot(x,y)
+plt.subplot(2, 3, 3)
+plt.plot(x, y)
 plt.title("Density of Inclination [Grad]")
 
-
-#Longitude of the ascending node
+# Longitude of the ascending node
 """ Winkel zwischen Referenzrichtung und Ascending Node-Richtung """
 prob_density = kde.gaussian_kde(ascending_node)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-x = np.linspace(min(ascending_node),max(ascending_node),300)
+x = np.linspace(min(ascending_node), max(ascending_node), 300)
 y = prob_density(x)
-plt.subplot(2,3,4)
-plt.plot(x,y)
+plt.subplot(2, 3, 4)
+plt.plot(x, y)
 plt.title("Density of Longitude of the \n ascending node [Grad]")
-
 
 # Argument of Perapsis
 """ Winkel der Periapsis bezogen auf Longitude of ascending node = Orientierung der "Spitze" der Elipse. Ob diese eher am weitesten von Referenzebene entfernt ist (90°/270°), oder in der Ebene liegt (0°/180°)) """
 prob_density = kde.gaussian_kde(arg_of_perapsis)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-x = np.linspace(min(arg_of_perapsis),max(arg_of_perapsis),300)
+x = np.linspace(min(arg_of_perapsis), max(arg_of_perapsis), 300)
 y = prob_density(x)
-plt.subplot(2,3,5)
-plt.plot(x,y)
+plt.subplot(2, 3, 5)
+plt.plot(x, y)
 plt.title("Density of Argument of Perapsis [Grad]")
 
-
-################Verteilung der Periodendauer 
+################Verteilung der Periodendauer
 # myasteroid = asteroids[3]
 # T = myasteroid.compute_period(epoch(5))
 # print('Periodendauer von %(name)s sind %(number)f Sekunden' %{"name": myasteroid.name, "number": T})
@@ -116,20 +109,20 @@ plt.title("Density of Argument of Perapsis [Grad]")
 period = []
 for asteroid in asteroids:
     period.append(
-        asteroid.compute_period(epoch(0))/(24*60*60)    #Sekunden in Tage umrechnen
-        )
+        asteroid.compute_period(epoch(0)) / (24 * 60 * 60)  # Sekunden in Tage umrechnen
+    )
 
 # Periodendauer einer Orbitumrundung:
 prob_density = kde.gaussian_kde(period)
-prob_density.covariance_factor = lambda : .05
+prob_density.covariance_factor = lambda: .05
 prob_density._compute_covariance()
 
-x = np.linspace(min(period),max(period),300)
+x = np.linspace(min(period), max(period), 300)
 y = prob_density(x)
-plt.subplot(2,3,6)
-plt.plot(x,y)
+plt.subplot(2, 3, 6)
+plt.plot(x, y)
 plt.title("Density of Orbit-Period [days]")
 
-#Plot anzeigen
+# Plot anzeigen
 plt.subplots_adjust(left=0.065, bottom=0.065, right=0.935, top=0.885, wspace=0.3, hspace=0.5)
 plt.show()
