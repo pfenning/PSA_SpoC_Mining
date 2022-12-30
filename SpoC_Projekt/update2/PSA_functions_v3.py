25e52c2c696af78fa758f7397e5ef00df3ba7f57
Basti <basti.pilatus@pimail-home.de>
2022-12-30 17:46:39 +0100

    Debugging klappt nicht

---
+++ SpoC_Projekt/update2/PSA_functions_v3.py
@@ -0,0 +1,215 @@
+import numpy as np
+import pykep as pk
+from pykep import phasing
+
+###########
+# Constants
+# ToDo: Eventuell anders gestalten. Noch unklar wie es bei unbekanntem Datensatz wird (aus einer Datei lesen?)
+###########
+T_START = pk.epoch_from_iso_string("30190302T000000")   # Start and end epochs
+T_END = pk.epoch_from_iso_string("30240302T000000")
+T_DAUER = 1827
+G = 6.67430e-11                                 # Cavendish constant (m^3/s^2/kg)
+SM = 1.989e30                                   # Sun_mass (kg)
+MS = 8.98266512e-2 * SM                         # Mass of the Trappist-1 star
+MU_TRAPPIST = G * MS                            # Mu of the Trappist-1 star
+DV_per_propellant = 10000                       # DV per propellant [m/s]
+TIME_TO_MINE_FULLY = 30                         # Maximum time to fully mine an asteroid
+
+
+def norm_bestand(bestand, material):
+    """
+    Gibt "normierten" Bestand zurück.
+    Für Rohstoffe: 1/max(Rohstoffe)
+    Für Tank: Tank
+    :param bestand: Aktueller bestand der 3 Rohstoffe und des Tanks
+    :param material: zu normierendes Material
+    :return: "normierter" Bestand
+    """
+    if material == 3:
+        return bestand[3]
+    elif max(bestand[:2]) == 0:
+        return 1
+    else:
+        return bestand[material]/max(bestand[:2])
+
+
+def get_dv(asteroid1, asteroid2, t_start, t_flug, print_result=False):
+    """
+    Berechnung des approximierten Delta V mithilfe des Lambert-Problems
+    :param asteroid1: Start-Asteroid
+    :param asteroid2: Lande-Asteroid
+    :param t_start: Startzeitpunkt
+    :param t_flug: Flugzeit
+    :param print_result: Ausgabe des Ergebnisses mit print()
+    :return: Delta V für berechnete Flugbahn
+    """
+    r1, v1 = asteroid1.eph(t_start)
+    r2, v2 = asteroid2.eph(t_start + t_flug)
+    # Solve the lambert problem for this flight
+    l = pk.lambert_problem(
+        r1=r1, r2=r2, tof=t_flug * pk.DAY2SEC, mu=MU_TRAPPIST, cw=False, max_revs=0
+    )
+    # Compute the delta-v necessary to go there and match its velocity
+    dv1 = [a - b for a, b in zip(v1, l.get_v1()[0])]
+    dv2 = [a - b for a, b in zip(v2, l.get_v2()[0])]
+    DV = np.linalg.norm(dv1) + np.linalg.norm(dv2)
+
+    if print_result:
+        print("Starttag:", f"{t_start:.0f}", "Flugzeit:", f"{t_flug:.0f}", " => Delta V =", f"{DV:.0f}")
+
+    return DV
+
+
+def clustering(knn, asteroids_kp, asteroid_1_idx, radius=4000):
+    """
+    Clusterbildung um Asteroid herum
+    :param knn: Cluster for "asteroids_kp"
+    :param asteroids_kp: Index-Liste von allen Asteroiden
+    :param asteroid_1_idx: Index vom aktuellen Asteroiden
+    :param radius: Begrenzung des Clusters auf max. Radius
+    :return: IDs der Nachbarn
+    """
+    neighb, neighb_inds, neighb_dis = knn.find_neighbours(asteroids_kp[asteroid_1_idx], query_type='ball', r=radius)
+    neighb_inds = list(neighb_inds)
+    # try:
+    #     neighb_inds.remove(asteroid_1_idx)
+    # except ValueError:
+    #     pass
+    return neighb_inds
+
+
+# ToDo: Zeitraum der Flugzeit neu definieren (z.B. auf 5-46 in 4er Schritten)
+#       - Reicht Auflösung? Sonst: nach gefundenem Minimum nochmal einen halben Schritt in jede Richtung machen
+def time_optimize(asteroid1, asteroid1_mas, asteroid1_mat,
+                  asteroid2, t_arr, t_opt, propellant=10000, print_result=False):
+    """
+    Zeitoptimierung von Delta V mit 2 Levels. Erst Flugzeit, dann Startzeit
+
+    Unterschied zu v1:
+    Betrachteter Startpunkt bewegt sich bis zu 60 Tage
+    Auswahl des Minimums über Gewichtung der Größen Flugzeit, Abbauzeit, Spritverbrauch
+
+    Übergabe: Asteroid 1 und 2, optimaler Startpunkt, optimale Abbauzeit auf aktuellem Asteroiden
+    Rückgabe: optimaler Startpunkt, optimale Flugzeit, optimiertes DV
+    :param propellant: Maximal erlaubter Tank
+    :param asteroid1_mas: Masse von Asteroid 1
+    :param asteroid1_mat: Material von Asteroid 1
+    :param asteroid1: Startasteroid
+    :param asteroid2: Landeasteroid
+    :param t_arr: Ankunftstag auf Asteroid 1
+    :param t_opt: Abbauzeit, um aktuellen Asteroid vollständig abzubauen
+    :param print_result: Ergebnisse von get_dv für das 2. Level der Optimierung ausgeben
+    :return: gewählten Starttag, gewählte Flugzeit, sicher ergebenes DV
+    """
+    dv_t_flug = []
+    dv_t_start = []
+    t_start = t_arr+t_opt
+    # Limit nach abbau, es werden mindestens 70 % abgebaut
+    if asteroid1_mat is 3:
+        limit = get_abbau_menge(propellant, asteroid1_mas, asteroid1_mat, 0.7*t_opt)
+    else:
+        limit = propellant
+    ###################################################
+    # Variation der Flugzeit, Startpunkt fest
+    ###################################################
+    # Mit der Suche wird am Tag begonnen, an dem der Start-Asteroid vollständig abgebaut ist.
+    t_flug_1 = range(5, 46, 4)
+
+    for t in t_flug_1:
+        dv_t_flug.append(get_dv(asteroid1, asteroid2, t_start, t))
+
+    # Minimum für Flugzeit heraussuchen (Gewichtung Flugzeit vs. DV)
+    if min(dv_t_flug)/DV_per_propellant > limit:  # Wenn bereits am Limit: Minimum nehmen
+        t_flug_min_dv = t_flug_1[dv_t_flug.index(min(dv_t_flug))]
+    else:                       # Ansonsten Gewichtung
+        results_t_flug = []
+        for i in range(0, len(t_flug_1)):
+            # "Normierung" für ähnliche Skalierung
+            # Zahlenfindung: Siehe MathTests.py
+            results_t_flug.append([t_flug_1[i] / 30, dv_t_flug[i] / 2000])
+        weights = np.array([0.3, 0.7])
+        rank_t_flug = []
+        for sol in results_t_flug:
+            rank_t_flug.append(sum(weights * sol))  # Bewertung aus gewichteter Summe
+
+        index_min = rank_t_flug.index(min(rank_t_flug))
+        # index_min = dv_t_flug.index(min(dv_t_flug))
+        t_flug_min_dv = t_flug_1[index_min]
+
+    ###################################################
+    # Variation des Startpunktes bei gegebener Flugzeit
+    # vor optimalem Starttag
+    ###################################################
+    t_start_relativ_var = [-0.3, -0.2, -0.1, -0.05]
+    t_start_var = []
+    for rel in t_start_relativ_var:
+        t_start_var.append(rel * t_opt)
+    # Nach optimalem Startpunkt
+    t_var_after = np.arange(0, 60 - t_opt, 4)
+    for t_var in t_var_after:
+        t_start_var.append(t_var)
+    # Berechnung für alle Variationen t_var
+    for t_var in t_start_var:
+        t = t_start + t_var
+        dv_t_start.append(get_dv(asteroid1, asteroid2, t, t_flug_min_dv, print_result))
+
+    # Minimum heraussuchen
+    if limit < min(dv_t_start)/DV_per_propellant:     # Wenn am Limit: Einfaches Minimum
+        index_min = dv_t_start.index(min(dv_t_start))
+    else:                           # Ansonsten Gewichtung
+        # Rangfolge bilden (da nicht unnötig lange Flugzeit gewählt werden sollte)
+        results_t_start = []
+        for i in range(0, len(t_start_var)):
+            # "Normierung" für ähnliche Skalierung - abs(t_var) da Betrag der Abweichung von t_opt relevant
+            # nur t_start, DV (t_flug bereits zuvor gewählt)
+            results_t_start.append([abs(t_start_var[i] / 10), dv_t_start[i] / 1000])
+
+        weights_neg_var = np.array([1.5, 0.7])
+        weights_pos_var = np.array([0.3, 0.7])
+        weights = weights_pos_var
+        rank_t_start = []
+        for i in range(0, len(results_t_start)):
+            if t_start_var[i] < 0:
+                weights = weights_neg_var
+            else:
+                weights = weights_pos_var
+            rank_t_start.append(sum(weights * results_t_start[i]))  # Bewertung aus gewichteter Summe
+
+        index_min = rank_t_start.index(min(rank_t_start))
+
+    # Wertepaar für Index des Minimums
+    t_m_min_dv = t_opt + t_start_var[index_min]
+    dv_min = dv_t_start[index_min]
+
+    return t_m_min_dv, t_flug_min_dv, dv_min
+
+
+def abbau(bestand, mass, material, t_aufenthalt):
+    """
+    Berechnung des abgebauten Materials und Bearbeitung von Bestand entsprechend
+    :param mass: Masse des Asteroiden
+    :param bestand: derzeitiger Bestand
+    :param material: Material-Index
+    :param t_aufenthalt: Aufenthaltsdauer auf Planet
+    :return:
+    """
+    bestand[material] = get_abbau_menge(bestand[material], mass, material, t_aufenthalt)
+
+
+def get_abbau_menge(material_bestand, mass, material, t_aufenthalt):
+    """
+    Berechnet die Abbaumenge für ein übergebenes Material und addiert es auf den Bestand
+    :param material_bestand: aktueller Bestand des Materials
+    :param mass: Masse des Asteroiden
+    :param material: Material-Typ
+    :param t_aufenthalt: Aufenthaltszeit auf Asteroiden
+    :return: neuer Bestand des Materials
+    """
+    if material == 3:
+        propellant_found = np.minimum(mass, (t_aufenthalt / TIME_TO_MINE_FULLY))
+        material_bestand = np.minimum(1.0, material_bestand + propellant_found)
+    else:
+        material_bestand += np.minimum(mass, (t_aufenthalt / TIME_TO_MINE_FULLY))
+
+    return material_bestand
---
+++ SpoC_Projekt/update2/branch_class.py
@@ -0,0 +1,259 @@
+import numpy as np
+import pykep as pk
+from pykep import phasing
+import copy
+from _Extra._Mathias.fuzzy_system import FuzzySystem
+import PSA_functions_v3 as psa
+
+
+
+class Branch:
+    #########################
+    # Static Class Constants
+    #########################
+    T_START = pk.epoch_from_iso_string("30190302T000000")  # Start and end epochs
+    T_END = pk.epoch_from_iso_string("30240302T000000")
+    T_DAUER = 1827
+    G = 6.67430e-11  # Cavendish constant (m^3/s^2/kg)
+    SM = 1.989e30  # Sun_mass (kg)
+    MS = 8.98266512e-2 * SM  # Mass of the Trappist-1 star
+    MU_TRAPPIST = G * MS  # Mu of the Trappist-1 star
+    DV_per_propellant = 10000  # DV per propellant [m/s]
+    TIME_TO_MINE_FULLY = 30  # Maximum time to fully mine an asteroid
+
+    #################
+    # ASTEROIDS:  Creating lists with indices of asteroids
+    #################
+
+    # Lists to be created
+    asteroids_kp = []
+    dict_asteroids = dict()     # "Beispiel Dictionary für neue Objekte"
+
+    # Loading data as keplerian elements (planets) in an "array"
+    data = np.loadtxt("SpoC_Datensatz.txt")
+    for line in data:
+        p = pk.planet.keplerian(
+            T_START,
+            (
+                line[1],
+                line[2],
+                line[3],
+                line[4],
+                line[5],
+                line[6],
+            ),
+            MU_TRAPPIST,
+            G * line[7],  # mass in planet is not used in UDP, instead separate array below
+            1,  # these variable are not relevant for this problem
+            1.1,  # these variable are not relevant for this problem
+            "Asteroid " + str(int(line[0])),
+        )
+        asteroids_kp.append(p)
+        dict_asteroids[int(line[0])] = [p, line[-2], int(line[-1])]  # Key = ID, Liste mit [Kaplerian, Masse, Material]
+    # Putzen - wahrscheinlich nicht notwendig
+    del p
+    del data
+
+    #########
+    # GÜTE
+    #########
+    verf = np.array([0.03, 0.4, 0.42, 0.15])  # ToDo: Verfügbarkeit berechnen
+
+    my_system = FuzzySystem(verf.min(), verf.max(), resolution=0.02)
+
+    ################
+    # Branch Objekt
+    ################
+    def __init__(self, i_start):
+        """
+        Erzeugt einen ganz neuen Pfad ausgehend einem übergebenden Startasteroiden
+        :param i_start: ID des Startasteroiden
+        """
+        self.visited = [{'id': i_start, 't_m': 0.0, 't_arr': 0.0, 'score last step': 0.0, 'branch score yet': 0.0}]
+        # Jedes Branch-Objekt hat eigene Visited-Asteroiden-Liste
+        self.not_visited = copy.deepcopy(Branch.dict_asteroids)
+        self.bestand = [0.0, 0.0, 0.0, 1.0]
+        self.asteroid_1_id = self.visited[0]['id']
+        self.asteroid_1_kp, self.asteroid_1_mas, self.asteroid_1_mat = self.not_visited[self.asteroid_1_id]
+        self.t_opt = 0.0
+
+    def _update_current_asteroid(self):
+        """
+        Setzt ID, Planet-Objekt, Masse, Material und optimale Abbauzeit auf Werte zuletzt besuchten Asteroids
+        :return:
+        """
+        self.asteroid_1_id = self.visited[-1]['id']
+        self.asteroid_1_kp, self.asteroid_1_mas, self.asteroid_1_mat = Branch.dict_asteroids[self.asteroid_1_id]
+        # Abbauzeit für gesamte Masse bestimmen
+        if self.asteroid_1_id == self.visited[0]['id']:
+            self.t_opt = 0
+        else:
+            self.t_opt = self.asteroid_1_mas*Branch.TIME_TO_MINE_FULLY
+
+    def _get_cluster_case(self):
+        """
+        Bestimmt den Cluster-Case
+        :return: Array von Material-Type-Arrays für Clusterbildung
+        """
+        sort_materials = sorted(self.bestand[0:3])
+        sorted_material_types = [self.bestand[0:3].index(value) for value in sort_materials]
+        # Fallunterscheidung
+        if self.bestand[3] < 0.4 and self.visited[-1]['t_arr'] < 1750:
+            # Sprit, ansonsten irgendein Rohstoff
+            cluster_iteration = [3]
+        elif 3*sort_materials[0] < sort_materials[1] and 3*sort_materials[1] < sort_materials[2]:
+            # geringste Verfügbarkeit, mittlere, häufigstes oder Sprit
+            cluster_iteration = [sorted_material_types[0], sorted_material_types[1], [sorted_material_types[2], 3]]
+        elif 3*sort_materials[0] < sort_materials[1]:
+            # geringste Verfügbarkeit, ansonsten Rest
+            cluster_iteration = [sorted_material_types[0], [sorted_material_types[1:], 3]]
+        elif 3*sort_materials[1] < sort_materials[2]:
+            # beiden geringsten, ansonsten Rest
+            cluster_iteration = [sorted_material_types[:2], [sorted_material_types[2], 3]]
+        else:
+            cluster_iteration = range(4)
+
+        return cluster_iteration
+
+    def _get_cluster_by_material(self, materials, radius=4000):
+        """
+        Erstellt Cluster für aktuellen Asteroiden aus allen Asteroiden, die übergebene Materialien besitzen
+        :param materials: int oder list of ints - Liste von Materialien, die geclustert werden sollen
+        :param radius: Cluster-Radius
+        :return: ASteroid-IDs der Nachbarasteroiden
+        """
+        if isinstance(materials, int):
+            materials = [materials]
+        candidates_id = [asteroid_id for asteroid_id, values in self.not_visited.items() if values[-1] in materials]
+        candidates = [self.not_visited[asteroid_id][0] for asteroid_id in candidates_id]
+
+        knn = phasing.knn(candidates, self.visited[-1]['t_arr'] + self.t_opt, 'orbital', T=30)
+        # neighb_inds = psa.clustering(knn, Branch.asteroids_kp, self.asteroid_1_id, radius)
+        neighb, neighb_inds, neighb_dis = knn.find_neighbours(Branch.asteroids_kp[self.asteroid_1_id],
+                                                              query_type='ball', r=radius)
+        neighb_inds = list(neighb_inds)
+        # ToDo: Eventuell knn anstatt ball?
+        return [candidates_id[index] for index in neighb_inds if candidates_id[index] != self.asteroid_1_id]
+
+    def get_next_possible_steps(self):
+        """
+        Bestimmt die Menge von Asteroiden, die für Expand-Schritt verwendet werden sollen.
+        - Cluster gewinnen
+        - Durch Cluster iterieren: siehe Vorversion (Zeitoptimierung, Score, SCORE PFAD)
+        - Prüfen, ob leere Menge, wenn ja Kandidaten erweitern und neu anfangen (Rekursion mit Hilfsfunktion?)
+        :return: {'t_m': Verweilzeit auf aktuellem ASteroiden
+                  'step': neue Zeile im Stil von Visited,
+                  'dv': DV für Schritt}
+        """
+        # Prüfen, ob noch ein Schritt notwendig
+        if Branch.T_DAUER-59 < self.visited[-1]['t_arr']:
+            self.visited[-1]['t_m'] = 60.0
+            raise StopIteration
+        # Update von Asteroid 1
+        self._update_current_asteroid()
+        # Speicher für mögliche Schritte
+        possible_steps = []
+        masses = []
+        # Cluster-Fall bestimmen
+        cluster_iteration = self._get_cluster_case()
+        # Durch Fälle iterieren, bis possible_steps nicht leer & Massen > 0.5, oder Cluster_Iteration fertig
+        for materials in cluster_iteration:
+            neighbour_ids = self._get_cluster_by_material(materials)
+            # Iteration durch Nachbar. Hinzufügen zu Menge, wenn erreichbar
+            for asteroid_2_id in neighbour_ids:
+                asteroid_2_kp, asteroid_2_mas, asteroid_2_mat = self.not_visited[asteroid_2_id]
+                t_m_opt_, t_flug_min_dv_, dv_min_ = psa.time_optimize(self.asteroid_1_kp,
+                                                                      self.asteroid_1_mas,
+                                                                      self.asteroid_1_mat,
+                                                                      asteroid_2_kp,
+                                                                      t_arr=self.visited[-1]['t_arr'],
+                                                                      t_opt=self.t_opt,
+                                                                      propellant=self.bestand[-1])
+                # Bewertung nur durchführen, wenn Asteroid auch erreichbar
+                if self.asteroid_1_mat is 3:
+                    limit = psa.get_abbau_menge(self.bestand[-1],
+                                                self.asteroid_1_mas,
+                                                self.asteroid_1_mat,
+                                                t_m_opt_)
+                else:
+                    limit = self.bestand[-1]
+                if (dv_min_ / Branch.DV_per_propellant) < limit:
+                    # Bewertung des Asteroids und des Wechsels
+                    score = Branch.my_system.calculate_score(  # ToDo: Über Normierung des delta_v sprechen
+                        # Tank nach Flug → dv muss normiert werden
+                        t_n=(limit - (dv_min_ / Branch.DV_per_propellant)),
+                        delta_v=(dv_min_ / 4000),  # Diese Normierung in Ordnung? - Dachte ganz sinnvoll
+                        bes=psa.norm_bestand(self.bestand, asteroid_2_mat),
+                        verf=self.verf[asteroid_2_mat],
+                        mas=asteroid_2_mas)
+                    # Alle Daten für Schritt speichern
+                    possible_steps.append(
+                        {'t_m': t_m_opt_,
+                         'step': {'id': asteroid_2_id,
+                                  't_m': 0.0,
+                                  't_arr': self.visited[-1]['t_arr']+t_m_opt_+t_flug_min_dv_,
+                                  'score last step': score,
+                                  'branch score yet': 0.0},
+                         'dv': dv_min_}
+                    )
+                    masses.append(asteroid_2_mas)
+            # Wenn possible_steps nicht mehr leer & Masse der möglichen Zielasteroiden größer 0.5
+            # → Mögliche Schritte gefunden, kann weiter gehen
+            if len(possible_steps) is not 0:
+                if max(masses) > 0.5:
+                    break
+        return possible_steps
+
+    def new_step(self, t_m, step, dv):
+        """
+        Führt neuen Schritt aus:
+        - berechnet t_m von aktuellem Asteroiden
+        - fügt Asteroiden zu Visited hinzu
+        - aktualisiert Bestand von Rohstoffen und Tank
+        - entfernt aktuellen Asteroiden aus Not_Visited
+        :return:
+        """
+        self._update_current_asteroid()     # ToDo: Notwendig? - zurzeit sicher sein, dass asteroid_1 aktuell ist
+        self.visited[-1]['t_m'] = t_m
+        self.visited.append(step)
+        # Abbau des Rohstoffs von Asteroid 1:
+        psa.abbau(self.bestand, self.asteroid_1_mas, self.asteroid_1_mat, t_m)
+        # Flug zum anderen Asteroiden
+        self.bestand[-1] -= dv / Branch.DV_per_propellant
+        self.not_visited.pop(self.asteroid_1_id)
+
+    def print_last_step(self):
+        print(f"Asteroid:{self.visited[-2]['id']}, Abbauzeit:{self.visited[-2]['t_m']:.0f}, "
+              f"Flugzeit: {self.visited[-2]['t_arr']:.0f}, Bestand bei Ankunft am neuen Asteroiden:", self.bestand,
+              f"Material:{Branch.dict_asteroids[self.visited[-2]['id']][-1]}")
+
+    def get_score(self):
+        """
+        Gibt Score des letzten Schritts zurück
+        :return: Score des letzten Schritts
+        """
+        return self.visited[-1]['score last step']
+
+    def get_result(self):
+        """
+        Erstellt die Lösungsvektoren
+        :return: ERG_a, ERG_t_m, ERG_t_arr
+        """
+        res_a = []
+        res_t_m = []
+        res_t_arr = []
+        res_list = [[out for out in step.values()] for step in self.visited]
+        for a, t_m, t_arr, _, _ in res_list:
+            res_a.append(a)
+            res_t_m.append(t_m)
+            res_t_arr.append(t_arr)
+        return res_a, res_t_m, res_t_arr
+
+    def print(self):
+        # print(self.visited)
+        res_list = [[out for out in step.values()] for step in self.visited]
+        for a, t_m, t_arr, _, _ in res_list:
+            print(f'ASteroid ID:{a}, Ankunftszeit:{t_arr:.0f}, Verweildauer:{t_m:.0f}')
+
+
+
