97859d8674b16490bdb81dbf2f5a2dfd267fdc4d
Pfenning_I <92577581+pfenning@users.noreply.github.com>
2023-01-12 18:06:13 +0100

    20230112

---
+++ _Extra/_Sebastian/time_optimize.py
@@ -0,0 +1,86 @@
+# Packages laden
+import numpy as np
+import pykep as pk
+# from pykep import epoch
+import matplotlib.pyplot as plt
+from scipy.stats import kde
+
+# Konstanten und Database Laden
+from spoc_constants import data, asteroids, asteroid_masses, asteroid_materials, MU_TRAPPIST
+
+
+def get_dv(asteroid1, asteroid2, t_start, dt_flug, print_result=False):
+    """ Berechnung des approximierten Delta V mithilfe des Lambert-Problems
+    
+        Übergabe:
+        asteroid1: Asteroid Objekt
+            Start-Asteroid
+        asteroid2: Asteroid Objekt
+            Lande-Asteroid
+        t_start: int, float (mjd_2000)
+            Startzeitpunkt
+        T: int, float
+            Flugzeit
+        printResult: boolean
+            Ausgabe des Ergebnisses mit print()
+
+        Rückgabe:
+        DV: float
+            Delta V für berechnete Flugbahn
+    """
+
+    r1, v1 = asteroid1.eph(t_start)
+    r2, v2 = asteroid2.eph(t_start + dt_flug)
+    # Solve the lambert problem for this flight
+    l = pk.lambert_problem(
+        r1=r1, r2=r2, tof=dt_flug * pk.DAY2SEC, mu=MU_TRAPPIST, cw=False, max_revs=0
+    )
+    # Compute the delta-v necessary to go there and match its velocity
+    dv1 = [a - b for a, b in zip(v1, l.get_v1()[0])]
+    dv2 = [a - b for a, b in zip(v2, l.get_v2()[0])]
+    DV = np.linalg.norm(dv1) + np.linalg.norm(dv2)
+
+    if print_result:
+        print("Starttag:", f"{t_start:.0f}", "Flugzeit:", f"{dt_flug:.0f}", " => Delta V =", f"{DV:.0f}")
+
+    return DV
+
+
+def time_optimize_time_v1(asteroid1, asteroid2, t_start, t_opt):
+    """ Zeitoptimierung von Delta V mit 2 Levels. Erst Flugzeit, dann Startzeit
+        Rechenaufwand: Anzahl Flugzeit-Elemente + Anzahl Startzeitpunkte (10+7=17)
+
+        Übergabe: Asteroid 1 und 2, optimaler Startpunkt, optimale Abbauzeit auf aktuellem Asteroiden
+        Rückgabe: optimaler Startpunkt, optimale Flugzeit, optimiertes DV
+    """
+    dv_tflug = []
+    dv_tstart = []
+    # Mit der Suche wird da begonnen, wo der Start-Asteroid vollständig abgebaut ist
+    # zunächst wird nur die Flugzeit optimiert in einem Bereich von 20-30 Tagen - Oben ist T mit 30 angegeben;
+    # könnte man auch ändern
+    dt_flug_1 = range(20, 60, 4)
+
+    # Variation der Flugzeit, Startpunkt fest
+    for t in dt_flug_1:
+        dv_tflug.append(get_dv(asteroid1, asteroid2, t_start, t))
+
+    # Minimum raussuchen
+    index_min = dv_tflug.index(min(dv_tflug))
+    dt_flug_min_dv = dt_flug_1[index_min]
+    dv_min = dv_tflug[index_min]
+    # print("Bestes Wertepaar: Flugzeit=", dt_flug_min_dv, "DV=", dv_min)
+
+    # Variation des Startpunktes bei gegebener Flugzeit
+    t_start_relativ_var = [-0.3, -0.2, -0.1, -0.05, 0.05, 0.1, 0.2, 0.3]
+    t_start_var = []
+    for rel in t_start_relativ_var: t_start_var.append(rel * t_start)
+    for t_var in t_start_var:
+        t = t_start + t_var
+        dv_tstart.append(get_dv(asteroid1, asteroid2, t, dt_flug_min_dv))
+
+    # Minimum raussuchen
+    index_min = dv_tstart.index(min(dv_tstart))
+    t_min_dv = t_start - t_start_var[index_min]
+    dv_min = dv_tstart[index_min]
+
+    return t_min_dv, dt_flug_min_dv, dv_min
---
+++ _Extra/_Sebastian/time_optimize_NSGA2.py
@@ -0,0 +1,155 @@
+# Packages laden
+import numpy as np
+import pykep as pk
+# from pykep import epoch
+import matplotlib.pyplot as plt
+from scipy.stats import kde
+
+# Konstanten und Database Laden
+from spoc_constants import data, asteroids, asteroid_masses, asteroid_materials, MU_TRAPPIST
+
+# DV berechnen
+from time_optimize import get_dv
+
+# pymoo Funktionen und Klassen
+from pymoo.core.problem import ElementwiseProblem
+# Algo
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+# Optimize
+from pymoo.optimize import minimize
+# Termination
+from pymoo.termination.default import DefaultSingleObjectiveTermination
+
+# "Globale" Objekte 
+# asteroid1 und asteroid2
+asteroid_trip = [asteroids[0], asteroids[0]]
+# Optimaler Starttag
+t_start_opt = 0
+
+
+class TimeOptimizeWithNSGA2(ElementwiseProblem):
+    def __init__(self, xl, xu):
+        """ 
+        Optimierungsproblem:
+        n_obj: Spritverbrauch soll minimiert werden
+        n_var: Variablen dabei sind: Flugzeit T, Starttag t_start
+        
+        Weitere mögliche Ziele:
+        Minimierung der Flugzeit,
+        Minimierung der Abweichung: absolut(t_start - t_start_opt)
+
+
+        Parameters:
+        xl : [t_start_min, T_min], np.array, float, int
+            Lower bounds for Startingtime and Flighttime. if integer all lower bounds are equal.
+
+        xu : [t_start_max, T_max], np.array, float, int
+            Upper bounds for Startingtime and Flighttime. if integer all upper bounds are equal.
+        """
+        super().__init__(n_var=2, n_obj=3, n_ieq_constr=0, xl=xl, xu=xu)
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        """
+        x: [t_start, T], NumPy Arry, int, float
+            t_start: Starttag des Wechsels (mjd_2000)
+            T: Flugzeit
+        out: Dictionary, output is written to
+        """
+
+        dv = get_dv(asteroid_trip[0], asteroid_trip[1], x[0], x[1]) / 1000
+        t_var = abs(x[0] - t_start_opt) / 30
+        t_opt = x[1] / 30
+
+        out["F"] = [t_var, t_opt, dv]
+
+
+def time_optimize_nsga2(asteroid_start, asteroid_landing, t_start, t_opt):
+    """ Zeitoptimierung von Delta V mit Hooke und Jeeves in vereinfachter Form
+  
+        Übergabe: 
+        asteroid_start: Asteroid-Objekt
+            Startasteroid
+        asteroid_landing: Asteroid-Objekt
+            Zielasteroid
+        t_start: int, float (mjd_2000)
+            optimaler Startpunkt (Masse komplett abgebaut)
+        t_opt: int, float
+            optimale Abbauzeit auf aktuellem Asteroiden
+        
+        Rückgabe:
+        tstart_min_dv: int,float
+            optimaler Startpunkt
+        t_flug_min_dv: int, float
+            optimale Flugzeit
+        dv_min: int,float
+            optimiertes DV
+    """
+
+    # Asteroiden festlegen
+    asteroid_trip[0] = asteroid_start
+    asteroid_trip[1] = asteroid_landing
+    # Optimale Startzeit speicher
+    t_start_opt = t_start
+
+    # Gültigkeitsbereich festlegen
+    t_var_min = -0.3 * t_opt  # Es müssen mindestens 60% abgebaut werden
+    t_var_max = 60 - t_opt  # Man darf maximal 60 Tage warten
+    t_start_min = t_start - t_var_min
+    t_start_max = t_start + t_var_max
+    t_flug_min = 1  # Flugzeit soll nicht kürzer als t_flug_min sein
+    t_flug_max = 60  # Flugzeit soll nicht länger als t_flug_max Tage betragen
+
+    # Ausgangspunkt (initial value)
+    # t_start = t_start
+    T = 30  # Sollte nochmal überlegt werden
+
+    # Problem erstellen
+    problem = TimeOptimizeWithNSGA2(np.array([t_start_min, t_flug_min]), np.array([t_start_max, t_flug_max]))
+
+    # Lösungsalgorithmus
+    algorithm = NSGA2(pop_size=10)  # Anzahl der Population und der Lösungen am Ende
+
+    # Termination Criterion
+    termination = DefaultSingleObjectiveTermination(
+        xtol=0.025,
+        # Minimale Schrittweite von --* Intervall Tage
+        # (z.B. xtol=0.025: Starttag [20-60] => Grenze: 1 Tag; Flugzeit: 1-100 Tage => 2.5 Tage)
+        cvtol=1e-6,  # Convergence in Constraings - wir haben keine Constraints
+        ftol=0.02,  # Minimale Änderung von --%
+        period=4,  # Betrachten der letzten -- Iterationen
+        n_max_gen=10,  # Maximale Anzahl "Generationen" - bei uns (wahrscheinlich neuer Ausgangspunkte)
+        n_max_evals=150  # Maximale Anzahl Funktionsaufrufe
+    )
+
+    # Optimize
+    res = minimize(
+        problem,
+        algorithm,
+        termination,
+        save_history=False,  # Kann später auskommentiert werden, nur für Testzwecke
+        verbose=True,  # print des Endergebnisses (auch auskommentieren)
+        return_least_infeasible=True  # Bestmögliche Lösung zurückgeben, falls sonst nichts gefunden wird
+    )
+
+    # Lösungsset auslesen
+    # tstart_min_dv, t_flug_min = res.X
+    # t_flug_min_dv = res.F[2]*30
+    # dv_min = res.F[0]*1000
+
+    print(res.X)
+    print(res.F)
+
+    # Multi-Criteria Decision-Making - make it easy
+    weights = np.array([0.2, 0.3, 0.5])  # t_start, T, DV
+    rank = []
+    for sol in res.F:
+        rank.append(sum(weights * sol))
+
+    opt_ind = rank.index(min(rank))
+    tstart_min_dv, t_flug_min_dv = res.X[opt_ind]
+    dv_min = res.F[opt_ind][2] * 1000
+    # print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
+    return tstart_min_dv, t_flug_min_dv, dv_min
