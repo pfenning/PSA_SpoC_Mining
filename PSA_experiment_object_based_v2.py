e1e335dc61dfb7a99fffe1c20979681de07f521c
Pfenning_I <92577581+pfenning@users.noreply.github.com>
2023-01-12 16:34:39 +0100

    -

--- PSA_experiment_object_based_v2.py
+++ PSA_experiment_object_based_v2.py
@@ -14,15 +14,13 @@ data = np.loadtxt("SpoC_Datensatz.txt")
 T_DAUER = 1827
 minutes = 10
 beta = 3
-
-# branch_v = bc.find_idx_start(data,0.001) # Vektor mit möglichen Startasteroiden
 branch_v = bc.find_idx_start(data, method='examples')
-minutes = 8
-beta = 4
-starting_branch = np.argpartition(starting_score, -4)[-4:]
-branch_v = []
-for line in starting_branch:
-    branch_v.append(starting_branches[line])
+
+# # branch_v = bc.find_idx_start(data,0.001) # Vektor mit möglichen Startasteroiden
+# starting_branch = np.argpartition(starting_score, -4)[-4:]
+# branch_v = []
+# for line in starting_branch:
+#     branch_v.append(starting_branches[line])
 
 print("branch_v done")
 
@@ -31,7 +29,7 @@ print(datetime.now(), end_time)
 
 beendete_Branches = []
 while datetime.now() < end_time:
-    v_done, top_beta = bc.beam_search(branch_v,beta, analysis='branch')    # analysis='branch'
+    v_done, top_beta = bc.beam_search(branch_v,beta)    # analysis='branch'
     if v_done:              # Fertige Lösungen gefunden
         beendete_Branches = np.concatenate((beendete_Branches, v_done), axis=0)
     if len(top_beta) == 0:  # Keine weiterzuführenden Lösungen gefunden
@@ -49,25 +47,44 @@ for branch in beendete_Branches:
     branch.print_summary()
 # print("beendete_Branches: ", beendete_Branches , len(beendete_Branches))
 
+# Chosing the best path
+final_branch = beendete_Branches[np.argmin([branch.get_guetemass() for branch in beendete_Branches])]
 
-i = 1
-score = []
-for final_branch in beendete_Branches:
-    score.append(final_branch.get_branch_score())
+# Lösungvektoren erzeugen
+final_branch.print()
+ERG_a, ERG_t_m, ERG_t_arr = final_branch.get_result()
 
-if len(score) > 10:
-    best_branch = np.argpartition(score, -10)[-10:] # Vektor mit den 10 besten Asteroiden
-else:
-    best_branch = range(len(beendete_Branches))
 
-i=1
-for solution in best_branch:
-    branch = beendete_Branches[solution]
-    # branch.print()
-    ERG_a, ERG_t_m, ERG_t_arr = branch.get_result()
+#################################################
+# Lösungszeitplan erstellen
+#################################################
+from from_website import SpoC_Kontrolle as SpoC
 
-    x = SpoC.convert_to_chromosome(ERG_t_arr + ERG_t_m + ERG_a)
-    print(SpoC.udp.pretty(x))
+x = SpoC.convert_to_chromosome(ERG_t_arr + ERG_t_m + ERG_a)
+print(SpoC.udp.pretty(x))
 
-    create_submission("spoc-mining","mine-the-belt",x,"TUDa_GoldRush_submission_file_"+ str(minutes) +"minutes_" + str(i) +".json","TUDa_GoldRush","submission_description")
-    i += 1
+from from_website.submisson_helper import create_submission
+create_submission("spoc-mining","mine-the-belt",x,"TUDa_GoldRush_submission_file_"+ str(minutes) +"minutes_" +".json","TUDa_GoldRush","submission_description")
+
+# from from_website import SpoC_Kontrolle as SpoC
+# from from_website.submisson_helper import create_submission
+# i = 1
+# score = []
+# for final_branch in beendete_Branches:
+#     score.append(final_branch.get_branch_score())
+#
+# if len(score) > 10:
+#     best_branch = np.argpartition(score, -10)[-10:] # Vektor mit den 10 besten Asteroiden
+# else:
+#     best_branch = range(len(beendete_Branches))
+#
+# for solution in best_branch:
+#     branch = beendete_Branches[solution]
+#     # branch.print()
+#     ERG_a, ERG_t_m, ERG_t_arr = branch.get_result()
+#
+#     x = SpoC.convert_to_chromosome(ERG_t_arr + ERG_t_m + ERG_a)
+#     print(SpoC.udp.pretty(x))
+#
+#     create_submission("spoc-mining","mine-the-belt",x,"TUDa_GoldRush_submission_file_"+ str(minutes) +"minutes_" + str(i) +".json","TUDa_GoldRush","submission_description")
+#     i += 1
