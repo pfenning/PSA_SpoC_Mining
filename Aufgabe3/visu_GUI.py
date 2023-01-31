f524e8c56205dadaee1f609f6cf711ad0a760908
Basti <basti.pilatus@pimail-home.de>
2023-01-31 19:53:00 +0100

    Aufgabe 3 und Solution.txt aktualisiert

--- Aufgabe3/visu_GUI.py
+++ Aufgabe3/visu_GUI.py
@@ -10,11 +10,12 @@ from tkinter import *
 import numpy as np
 from TripInSpace import TripInSpace
 
+
+############## EINGABE ##############
+solution = np.array([0.0, 16.0, 62.45105066575198, 135.43351830918306, 184.35326981635265, 230.9121897504506, 288.2350258224929, 319.178306733055, 361.2828297752968, 404.342770222788, 453.36195568021793, 507.05309623337564, 546.7200918140377, 594.5652909975905, 636.4141983404985, 711.5399832226511, 758.894890637341, 804.7590069285344, 838.55993255801, 879.0295433554976, 947.8604984547422, 989.7780689814829, 1041.5859434873178, 1085.0224636797, 1142.5656157273322, 1188.1309648564277, 1236.9839401733548, 1277.2287040933024, 1314.3836245275645, 1348.3731730850525, 1393.0588232971018, 1439.6146060580775, 1485.9703361877484, 1526.6954244259296, 1573.6006820795346, 1624.858639800528, 1674.5390655073606, 1710.7106354242333, 1753.849975216157, 1789.9766156767644, 0.0, 24.45105066575198, 46.98246764343108, 20.919751507169583, 18.55891993409795, 29.322836072042275, 14.943280910562102, 22.104523042241794, 29.05994044749116, 27.019185457429966, 29.691140553157695, 29.666995580662036, 21.845199183552847, 21.84890734290797, 47.125784882152644, 29.354907414689773, 23.864116291193472, 17.800925629475664, 28.469610797487555, 40.830955099244605, 25.91757052674068, 23.807874505834942, 23.436520192382098, 29.543152047632216, 29.56534912909546, 24.852975316927076, 26.244763919947747, 23.15492043426198, 19.989548557487986, 26.685650212049367, 26.55578276097568, 26.35573012967087, 24.72508823818114, 28.905257653605187, 29.257957720993403, 27.6804257068326, 18.171569916872656, 29.139339791923593, 26.126640460607362, 37.02338432323563, 2447.0, 3385.0, 3979.0, 735.0, 4618.0, 6781.0, 4056.0, 7316.0, 3413.0, 2992.0, 10.0, 9187.0, 1444.0, 7060.0, 4433.0, 854.0, 2515.0, 4771.0, 4802.0, 1839.0, 6867.0, 6747.0, 5694.0, 8547.0, 390.0, 1844.0, 2630.0, 5231.0, 4784.0, 1545.0, 4121.0, 4069.0, 4204.0, 394.0, 6358.0, 9334.0, 9368.0, 5568.0, 1038.0, 4087.0, ])
+
+
 ########### Trip initialisieren ############
-# with open("Solution.txt","r") as tf:
-#     solution = np.array(tf.read()) # np.loadtxt("Solution.txt")
-# solution = np.loadtxt("Solution.txt", delimiter=",")
-solution = np.array([0.0, 20.0, 71.28207075521709, 119.03880397768823, 156.8729508200197, 203.5609882194199, 244.69382428787384, 285.038710961449, 329.1510636770093, 379.23654964528663, 436.4407344403058, 472.2471685231399, 514.219590609267, 551.9780100820611, 575.1419605512108, 617.9189642078104, 683.3451634457787, 717.578717741857, 752.5658138338164, 805.0688657178272, 851.6554303070845, 914.1194763554862, 950.6907559772899, 981.3403749440956, 1029.1026493833601, 1077.9268690949098, 1128.0758077810108, 1168.9933193745449, 1204.660086270053, 1261.3226381335446, 1296.4219234820632, 1349.1549173163844, 1398.3303688176732, 1431.499641381145, 1474.212693931286, 1519.0129230203136, 1559.933651238005, 1601.5674113816087, 1649.7089937738408, 1694.5215690911004, 1739.707201994975, 1785.883958072634, 0.0, 27.28207075521709, 29.75673322247114, 27.834146842331478, 26.68803739940021, 23.132836068453948, 26.344886673575143, 28.112352715560267, 24.085485968277332, 31.204184795019184, 23.80643408283411, 17.972422086127086, 29.75841947279408, 7.163950469149648, 16.77700365659967, 37.42619923796828, 28.233554296078264, 22.98709609195938, 24.50305188401082, 28.586564589257367, 34.46404604840161, 28.571279621803683, 6.64961896680579, 27.762274439264335, 20.824219711549656, 24.148938686101065, 28.917511593534144, 23.66676689550811, 28.662551863491668, 23.0992853485186, 28.732993834321125, 29.175451501288684, 19.169272563471914, 26.713052550140944, 18.800229089027653, 24.92072821769155, 21.633760143603585, 28.141582392232063, 16.812575317259565, 27.185632903874662, 24.176756077658833, 41.11604192736604, 9577.0, 831.0, 368.0, 8861.0, 1470.0, 7146.0, 145.0, 1219.0, 4905.0, 3764.0, 8387.0, 6928.0, 4532.0, 6323.0, 5254.0, 8844.0, 740.0, 6374.0, 7521.0, 7875.0, 7588.0, 8312.0, 8431.0, 8168.0, 4076.0, 2179.0, 6416.0, 5677.0, 1454.0, 6936.0, 1751.0, 9524.0, 9197.0, 6018.0, 1666.0, 4370.0, 5111.0, 9644.0, 5257.0, 5351.0, 7523.0, 4315.0, ])
 idx_split1 = int(len(solution)/3)
 idx_split2 = 2*idx_split1
 t_arr_test = solution[:idx_split1]
@@ -67,7 +68,7 @@ figure3 = Figure(figsize=(4, 3), dpi=100)
 ax3 = figure3.add_subplot(111)
 canvas3 = FigureCanvasTkAgg(figure3, master=root)
 canvas3.draw()
-canvas3.get_tk_widget().grid(row=2, column=0,pady=15,rowspan=8)
+canvas3.get_tk_widget().grid(row=2, column=0,pady=15,rowspan=9)
 
 # Create two labels to display text
 Font_head = ("Cambria", 18, "bold")
@@ -86,45 +87,58 @@ text4.configure(font = Font_small, background = "white" )
 text5 = tk.Label(root, text="  -  ", relief="groove" , bd=2)
 text5.grid(row=4, column=5)
 text5.configure(font = Font_small, background = "white" ,width=4,height=1)
+text12 = tk.Label(root, text="Abgebaute Material")
+text12.grid(row=5, column=4,sticky='e')
+text12.configure(font = Font_small, background = "white" )
+text13 = tk.Label(root, text="  -  ", relief="groove" , bd=2)
+text13.grid(row=5, column=5)
+text13.configure(font = Font_small, background = "white" ,width=4,height=1)
+text14 = tk.Label(root, text="Hinterlassene Material")
+text14.grid(row=6, column=4,sticky='e')
+text14.configure(font = Font_small, background = "white" )
+text15 = tk.Label(root, text="  -  ", relief="groove" , bd=2)
+text15.grid(row=6, column=5)
+text15.configure(font = Font_small, background = "white" ,width=4,height=1)
+
 
 text6 = tk.Label(root, text="Nächster Asteroid")
-text6.grid(row=5, column=4,sticky='e')
+text6.grid(row=8, column=4,sticky='e')
 text6.configure(font = Font_middle, background = "white" )
 text7 = tk.Label(root, text="-", relief="groove" , bd=2)
-text7.grid(row=5, column=5)
+text7.grid(row=8, column=5)
 text7.configure(font = Font_small, background = "white" ,width=4,height=1)
 text8 = tk.Label(root, text="Material")
-text8.grid(row=6, column=4,sticky='e')
+text8.grid(row=9, column=4,sticky='e')
 text8.configure(font = Font_small, background = "white" )
 text9 = tk.Label(root, text="-", relief="groove" , bd=2)
-text9.grid(row=6, column=5)
+text9.grid(row=9, column=5)
 text9.configure(font = Font_small, background = "white",width=4,height=1 )
 text10 = tk.Label(root, text="Time of Flight")
-text10.grid(row=7, column=4,sticky='e')
+text10.grid(row=10, column=4,sticky='e')
 text10.configure(font = Font_small, background = "white" )
 text11 = tk.Label(root, text="-", relief="groove" , bd=2)
-text11.grid(row=7, column=5)
+text11.grid(row=10, column=5)
 text11.configure(font = Font_small, background = "white",width=4,height=1 )
 
 score_1 = tk.Label(root, text="Die aktuelle Güte ist")
 score_1.grid(row=3, column=1,pady = 5,columnspan=3,sticky="nsew")
 score_1.configure(font = Font_head, background = "white" )
 score_2 = tk.Label(root, text="  -  ", relief="groove" , bd=2)
-score_2.grid(row=4, column=2,pady=15,sticky="nsew")
+score_2.grid(row=4, column=2,pady=15,sticky="nsew",rowspan=3)
 score_2.configure(font = Font_middle, background = "green",width=5,height=2 )
 
 # Create two buttons in row 1 that occupy the same column span
-text1 = tk.Label(root, text="Wählen sie den Schritt")
-text1.grid(row=6, column=1, pady=5,columnspan=3,sticky="nsew")
+text1 = tk.Label(root, text="Wählen Sie den Schritt")
+text1.grid(row=8, column=1, pady=5,columnspan=3,sticky="nsew")
 text1.configure(font = Font_head, background = "white" )
 
 button1 = Button(root, text="<--", command=lambda: update_gui(1))
-button1.grid(row=7, column=1,sticky="nsew", padx=5, pady=5)
+button1.grid(row=9, column=1,sticky="nsew", padx=5, pady=5,rowspan=2)
 button2 = Button(root, text="-->", command=lambda: update_gui(2))
-button2.grid(row=7, column=3,sticky="nsew", padx=5, pady=5)
+button2.grid(row=9, column=3,sticky="nsew", padx=5, pady=5,rowspan=2)
 
 button_text = tk.Label(root, text= schritt_wert, relief="groove" , bd=5)
-button_text.grid(row=7, column=2)
+button_text.grid(row=9, column=2,rowspan=2)
 button_text.configure(font = Font_small, background = "white",width=15,height=2 )
 
 def update_values(event,schritt):
@@ -132,6 +146,9 @@ def update_values(event,schritt):
     current_ast = str(a_test[schritt])
     text3.config(text=current_ast)
     text5.config(text=f"{trip.get_material(schritt)}")
+    text13.config(text=f"{trip.get_mined_material(schritt):.1f}")
+    text15.config(text=f"{trip.get_missed_material(schritt):.1f}")
+
     if schritt+1 < len(a_test):
         next_ast = str(a_test[schritt+1])
         text7.config(text=next_ast)
@@ -141,6 +158,9 @@ def update_values(event,schritt):
         text7.config(text=" - ")
         text9.config(text=" - ")
         text11.config(text=" - ")
+        text13.config(text=" - ")
+        text15.config(text=" - ")
+
     score_2.config(text = f"{trip.get_score(schritt):.3}")
 
 def update_gui(button_num):
@@ -161,5 +181,8 @@ def update_gui(button_num):
     button_text.config(text=schritt_wert)
     update_figures(None, schritt_wert)
     update_values(None, schritt_wert)
+
+update_figures(None, 0)
+update_values(None, 0)
 
 root.mainloop()
